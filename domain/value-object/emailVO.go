d365c23cf594c90a08c94716683dcfcc1f610479
Suzuka Funayama <suzuka-funayama@kalonade.co.jp>
2025-11-10 18:34:17 +0900

    add user value-object

---
+++ domain/value-object/emailVO.go
@@ -0,0 +1,22 @@
+package value_object
+
+import (
+	"errors"
+	"regexp"
+)
+
+type Email struct {
+	value string
+}
+
+func FromStringEmail(v string) (Email, error) {
+	regex := regexp.MustCompile(`^[\w._%+\-]+@[\w.\-]+\.[A-Za-z]{2,}$`)
+	if !regex.MatchString(v) {
+		return Email{}, errors.New("invalid email format")
+	}
+	return Email{value: v}, nil
+}
+
+func (e Email) Value() string {
+	return e.value
+}
